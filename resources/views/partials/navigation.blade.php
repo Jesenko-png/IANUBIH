c65147dae47526d37059ee10f85d386d1a3be6f3
Jesenko-png <jesenko9@hotmail.com>
2026-08-08 16:00:51 +0200

    font adjustment

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -4,21 +4,6 @@
 @endphp
 
 <nav class="navbar navbar-fixed-top custom-navbar" role="navigation" aria-label="{{ __('navigation.home') }}">
-    <div class="institution-bar hidden-xs">
-        <div class="container institution-bar-inner">
-            <span>{{ __('home.topbar.institution') }}</span>
-            <div class="institution-tools">
-                <a href="{{ route('people', ['locale' => app()->getLocale()]) }}">
-                    <i class="fa fa-search" aria-hidden="true"></i>
-                    {{ __('navigation.search') }}
-                </a>
-                <span class="institution-divider" aria-hidden="true"></span>
-                <a href="{{ route($currentRouteName, array_merge($currentRouteParameters, ['locale' => 'bs'])) }}" @class(['active' => app()->getLocale() === 'bs']) lang="bs">BHS</a>
-                <a href="{{ route($currentRouteName, array_merge($currentRouteParameters, ['locale' => 'en'])) }}" @class(['active' => app()->getLocale() === 'en']) lang="en">ENG</a>
-            </div>
-        </div>
-    </div>
-
     <div class="container navbar-main">
         <div class="navbar-header">
             <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse" aria-label="Menu">
@@ -26,6 +11,10 @@
                 <span class="icon icon-bar"></span>
                 <span class="icon icon-bar"></span>
             </button>
+
+            <span class="navbar-institution-name hidden-xs hidden-sm hidden-md">
+                {{ __('home.topbar.institution') }}
+            </span>
 
             <a href="{{ route('home', ['locale' => app()->getLocale()]) }}" class="navbar-brand">
                 <img src="{{ asset('assets/new-event/images/logo.jpg') }}" class="navbar-brand-logo" alt="IANUBIH logo">
@@ -42,7 +31,19 @@
                 <li @class(['active' => request()->routeIs('news')])><a href="{{ route('news', ['locale' => app()->getLocale()]) }}">{{ __('navigation.news') }}</a></li>
                 <li @class(['active' => request()->routeIs('cooperation')])><a href="{{ route('cooperation', ['locale' => app()->getLocale()]) }}">{{ __('navigation.cooperation') }}</a></li>
                 <li @class(['active' => request()->routeIs('contact')])><a href="{{ route('contact', ['locale' => app()->getLocale()]) }}" class="nav-contact-link">{{ __('navigation.contact') }}</a></li>
-                <li class="visible-xs mobile-language">
+                <li class="navbar-utility navbar-search hidden-xs hidden-sm">
+                    <a href="{{ route('people', ['locale' => app()->getLocale()]) }}">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                        {{ __('navigation.search') }}
+                    </a>
+                </li>
+                <li class="navbar-utility navbar-language hidden-xs hidden-sm">
+                    <a href="{{ route($currentRouteName, array_merge($currentRouteParameters, ['locale' => 'bs'])) }}" @class(['active' => app()->getLocale() === 'bs']) lang="bs">BHS</a>
+                </li>
+                <li class="navbar-utility navbar-language hidden-xs hidden-sm">
+                    <a href="{{ route($currentRouteName, array_merge($currentRouteParameters, ['locale' => 'en'])) }}" @class(['active' => app()->getLocale() === 'en']) lang="en">ENG</a>
+                </li>
+                <li class="visible-xs visible-sm mobile-language">
                     <a href="{{ route($currentRouteName, array_merge($currentRouteParameters, ['locale' => app()->getLocale() === 'bs' ? 'en' : 'bs'])) }}">
                         {{ app()->getLocale() === 'bs' ? 'ENG' : 'BHS' }}
                     </a>
