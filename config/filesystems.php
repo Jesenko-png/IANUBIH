b6ebd52b9a99a6e69f1099ba3e3bd343eb8146fd
Jesenko-png <jesenko9@hotmail.com>
2026-08-12 18:56:37 +0200

    datebase path

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,8 +40,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => rtrim(env('APP_URL', 'http://localhost'), '/').'/storage',
+            'root' => public_path('uploads'),
+            'url' => '/uploads',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
